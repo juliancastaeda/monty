164c7429850920111faa1d131fddc3fc22896e5c
juliancastaeda <1277@holbertonschool.com>
2020-01-04 08:29:02 -0500

    load file

---
+++ functions_Stacks_Queues.c
@@ -0,0 +1,124 @@
+#include "monty.h"
+/**
+ * push - adition new node
+ * @stack: new node
+ * @n: init double linked lists
+ *
+ * Return: new element or NULL
+ **/
+void push(stack_t **stack, unsigned int h)
+{
+stack_t *new;
+
+new = malloc(sizeof(new));
+if (new == NULL)
+exit(EXIT_FAILURE);
+if (*stack == NULL)
+{
+*stack = new;
+(*stack)->prev = NULL;
+(*stack)->next = NULL;
+(*stack)->n = h;
+}
+else
+{
+new->next = *stack;
+(*stack)->prev = new;
+new->prev = NULL;
+new->n = h;
+*stack = new;
+}
+}
+
+/**
+ * pall - print d_listint
+ * @h: double listen
+ *
+ * 
+ */
+void pall(stack_t **stack, unsigned int h)
+{
+size_t a = 0;
+h = h;
+if (!= stack)
+exit(EXIT_SUCCES);
+for (a = 0; *stack != NULL; a++)
+{
+printf("%d\n", (*stack)->n);
+*stack = (*stack)->next;
+}
+}
+
+/**
+ * pint - print date
+ * @stack : pointer
+ * @value : int of input
+ * @stack : pointer
+ */
+
+void pint(stack_t **stack, unsigned int h)
+{
+if (!stack)
+{
+fprintf(stderr, "L%u: can't pint, stack empty\n", h);
+exit(EXIT_FAILURE);
+}
+else
+printf("%d\n", (*stack)->n);
+}
+
+/**
+ * pop - Function that remove the top element of the stack
+ * @stack : pointer of input
+ * @value : int of input
+ * @stack : double pointer of input
+ */
+
+void pop(stack_t **stack, unsigned int h)
+{
+stack_t *new;
+
+if (stack == NULL)
+{
+fprintf(stderr, "L%u: can't pop an empty stack\n", h);
+exit(EXIT_FAILURE);
+}
+else
+{
+new = *stack;
+*stack = new->next;
+new->next = NULL;
+(*stack)->prev = NULL;
+free(new);
+}
+}
+
+/**
+ * swap - print date
+ * @h : int 
+ * @stack : pointer
+ */
+
+void swap(stack_t **stack, unsigned int h)
+{
+stack_t *new = NULL;
+int a = 0, i = 0;
+
+while ((*stack)->next != NULL)
+{
+*stack = (*stack)->next;
+i++;
+}
+if (i < 2)
+{
+fprintf(stderr, "L%u: can't swap, stack too short\n", h);
+exit(EXIT_FAILURE);
+}
+else
+{
+new = new->next;
+a = new->n;
+new->n = (*stack)->n;
+(*stack)->n = a;
+}
+}
